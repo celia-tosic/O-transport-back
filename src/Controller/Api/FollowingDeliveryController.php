ccfb36b6d9ee6f685dccef82cabec94ad1d08ff5
Vincent-Naulleau <vincent.naulleau2013@gmail.com>
2022-04-06 13:48:41 +0200

    [FOLLOWINGDELIVERY]Route testing shoot

--- src/Controller/Api/FollowingDeliveryController.php
+++ src/Controller/Api/FollowingDeliveryController.php
@@ -7,12 +7,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * 
+ * @Route("/api/drivers", name="app_following_delivery")
+ */
 class FollowingDeliveryController extends AbstractController
 {
     /**
-     * @Route("/api/drivers/deliveries", name="app_following_delivery")
-     */
-    public function index(DeliveryRepository $deliveryRepository): Response {
+     * showDeliveries
+     * @Route("/{id}/deliveries", name="showDelivery", methods="GET", requirements={"id"="\d+"})
+     * @return Response
+    */
+    public function showDeliveries(DeliveryRepository $deliveryRepository): Response {
         $deliveryList = $deliveryRepository->findAllDeliveriesByDriver();
     
         return $this->json($deliveryList, Response::HTTP_OK, [], ['groups'=>"api_driver_deliveries"]);
