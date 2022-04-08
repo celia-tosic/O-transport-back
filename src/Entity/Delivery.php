e201eb69b349b7cc77b9d277fa9b54b160f0f8eb
Baltidus <ludovici.joffrey@gmail.com>
2022-04-08 14:32:54 +0200

    [DELIVERY DETAILS] Add created at and update to show

--- src/Entity/Delivery.php
+++ src/Entity/Delivery.php
@@ -58,11 +58,13 @@ class Delivery
 
     /**
      * @ORM\Column(type="datetime")
+     * @Groups("api_deliveries_details")
      */
     private $created_at;
 
     /**
      * @ORM\Column(type="datetime", nullable=true)
+     * @Groups("api_deliveries_details")
      */
     private $updated_at;
 
