07d17f737636696230387158868f584e44cd9750
celia-tosic <celiatosic@gmail.com>
2022-04-12 22:25:29 +0200

    [AUTHENTICATION] add class to send user informations with JWT token

---
+++ src/EventListener/AuthenticationSuccessListener.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Entity\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+
+class AuthenticationSuccessListener
+{
+
+/**
+ * @param AuthenticationSuccessEvent $event
+ */
+    public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
+    {
+        $data = $event->getData();
+        $user = $event->getUser();
+
+        // dd($user);
+
+        if (!$user instanceof User) {
+            return;
+        }
+
+        $data['user'] = array(
+            'id' => $user->getId(),
+            'firstname' => $user->getFirstName(),
+            'roles' => $user->getRoles(),
+        );
+
+        $event->setData($data);
+    }
+}
