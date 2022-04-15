f18d37af9da98b1ee3c817bc6a1324f92d9bfe8e
Baltidus <ludovici.joffrey@gmail.com>
2022-04-15 11:23:44 +0200

    [FIXTURES] Change algorithm : Now a driver cannot be in status 1 on severals deliveries wich are aswell in status 1

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -44,12 +44,12 @@ class AppFixtures extends Fixture
         $customers[] = $bricodepot;
         $customers[] = $mrBricolage;
         $customers[] = $bricomarche;
-        
+
         $customersObjects = [];
 
-       
+
         // Boucle de création d'objet "Customer"
-        foreach ($customers as $currentCustomer ) {
+        foreach ($customers as $currentCustomer) {
 
             // On fabrique les nouveaux objets customers
             $customer = new Customer();
@@ -57,63 +57,62 @@ class AppFixtures extends Fixture
             $customer->setAddress($currentCustomer['address']);
             $customer->setPhoneNumber($faker->mobileNumber());
             $customersObjects[] = $customer;
-            
-            $manager->persist($customer);
 
+            $manager->persist($customer);
         }
-        
+
         // --- USERS ---
 
         //Drivers
         $sylvain = [
-            'email' => 'sylvain@driver.com', 
-            'roles' => ['ROLE_DRIVER'], 
+            'email' => 'sylvain@driver.com',
+            'roles' => ['ROLE_DRIVER'],
             'password' => '$2y$13$5Nszt9g3Ujlyr9SbRlSuJes0aYdx/ceG/CkW7dwpvjqvFSocSjM/C',
-            'firstname' => 'Sylvain', 
-            'lastname' => 'Danlaitan', 
+            'firstname' => 'Sylvain',
+            'lastname' => 'Danlaitan',
             'status' => 0,
         ];
 
         $yves = [
-            'email' => 'yves@driver.com', 
+            'email' => 'yves@driver.com',
             'roles' => ['ROLE_DRIVER'],
             'password' => '$2y$13$5Nszt9g3Ujlyr9SbRlSuJes0aYdx/ceG/CkW7dwpvjqvFSocSjM/C',
-            'firstname' => 'Yves', 
-            'lastname' => 'Atrovite', 
+            'firstname' => 'Yves',
+            'lastname' => 'Atrovite',
             'status' => 0,
         ];
 
         $francois = [
-            'email' => 'francois@driver.com', 
+            'email' => 'francois@driver.com',
             'roles' => ['ROLE_DRIVER'],
             'password' => '$2y$13$5Nszt9g3Ujlyr9SbRlSuJes0aYdx/ceG/CkW7dwpvjqvFSocSjM/C',
-            'firstname' => 'François', 
-            'lastname' => 'Papraissé', 
+            'firstname' => 'François',
+            'lastname' => 'Papraissé',
             'status' => 2,
         ];
 
         $alphonse = [
-            'email' => 'alphonse@driver.com', 
+            'email' => 'alphonse@driver.com',
             'roles' => ['ROLE_DRIVER'],
             'password' => '$2y$13$5Nszt9g3Ujlyr9SbRlSuJes0aYdx/ceG/CkW7dwpvjqvFSocSjM/C',
-            'firstname' => 'Alphonse', 
-            'lastname' => 'Danlmur', 
+            'firstname' => 'Alphonse',
+            'lastname' => 'Danlmur',
             'status' => 0,
         ];
 
         $romain = [
-            'email' => 'romain@admin.com', 
+            'email' => 'romain@admin.com',
             'roles' => ['ROLE_ADMIN'],
             'password' => '$2y$13$sUv.tOxU2KXZAxiBUFAvReWV/TG87EDsNYChWjp1GPKZOQ7dSjCt.',
-            'firstname' => 'Romain', 
-            'lastname' => 'Defer', 
+            'firstname' => 'Romain',
+            'lastname' => 'Defer',
             'status' => null,
         ];
 
         //Admin
         $loic = [
             'email' => 'loic@admin.com',
-            'roles' =>['ROLE_ADMIN'],
+            'roles' => ['ROLE_ADMIN'],
             'password' => '$2y$13$sUv.tOxU2KXZAxiBUFAvReWV/TG87EDsNYChWjp1GPKZOQ7dSjCt.',
             'firstname' => 'Loic',
             'lastname' => 'Omprampa',
@@ -127,54 +126,77 @@ class AppFixtures extends Fixture
         $users[] = $loic;
         $users[] = $romain;
         $userObjects = [];
+        $userObjects[0] = null;
 
         foreach ($users as $currentUser) {
-            $user = new User; 
-            $user->setEmail($currentUser['email']); 
-            $user->setRoles($currentUser['roles']); 
-            $user->setPassword($currentUser['password']); 
-            $user->setFirstname($currentUser['firstname']); 
-            $user->setLastname($currentUser['lastname']); 
+            $user = new User;
+            $user->setEmail($currentUser['email']);
+            $user->setRoles($currentUser['roles']);
+            $user->setPassword($currentUser['password']);
+            $user->setFirstname($currentUser['firstname']);
+            $user->setLastname($currentUser['lastname']);
             $user->setPhoneNumber($faker->mobileNumber());
-            if ($currentUser['status'] !== null ){
-                $user->setStatus($currentUser['status']);  
-            } 
+            if ($currentUser['status'] !== null) {
+                $user->setStatus($currentUser['status']);
+            }
 
             $userObjects[] = $user;
-            
+
             $manager->persist($user);
         }
-        
-    
+
+
         // --- DELIVERIES ---
-        
+
         $nbDeliveries = 10;
 
         $merchandises = [
             'sable', 'béton', 'gravier', 'bois', 'acier', 'aluminium'
-        ]; 
+        ];
 
-        
-        
+
+
         for ($i = 0; $i < $nbDeliveries; $i++) {
-
-            $deliveryObject = [];
+            // On créé une nouvelle livraison
             $delivery = new Delivery();
-            $delivery->setStatus($faker->numberBetween(0, 2));
-            $merchandisesCount = count($merchandises)-1;
+            // On randomise le choix de la marchandise
+            $merchandisesCount = count($merchandises) - 1;
             $merchandisesIndex = rand(0, $merchandisesCount);
+            // On affecte la marchandise
             $delivery->setMerchandise($merchandises[$merchandisesIndex]);
-            $delivery->setVolume($faker->numberBetween(0,20));
+            // On génène un volume aléatoire avec Faker
+            $delivery->setVolume($faker->numberBetween(0, 20));
+            // Idem avec le commentaire
             $delivery->setComment($faker->sentence());
-            $delivery->setCreatedAt(new DateTime());
+            // On y affecte un client aléatoire
             $customerIndex = rand(0, 3);
             $randomCustomer = $customersObjects[$customerIndex];
             $delivery->setCustomer($randomCustomer);
-            $delivery->setAdmin($userObjects[rand(4,5)]);
-            $driverIndex = rand(0, 3);
-            $delivery->setDriver($userObjects[$driverIndex]);
 
-            $manager->persist($delivery); 
+            // On attribue un administrateur
+            $delivery->setAdmin($userObjects[rand(5, 6)]);
+            $delivery->setStatus($faker->numberBetween(0, 2));
+            if ($delivery->getStatus() == 2) {
+                // Si la livraison est terminé, elle a forcément un chauffeur
+                $driverIndex = rand(1, 4);
+                $delivery->setDriver($userObjects[$driverIndex]);
+            } else {
+                $driverIndex = rand(0, 4);
+                $delivery->setDriver($userObjects[$driverIndex]);
+                //Si un driver a été affecté,
+                if ($delivery->getDriver() != null) {
+                    // Si le status du driver est déjà à 1, le status de la livraison est toujours à 0
+                    if ($delivery->getDriver()->getstatus() === 1) {
+                        $delivery->setStatus(0);
+                    } else {
+                        // Sinon la livraison est en cours donc on met la livraison et le status du driver a 1
+                        $delivery->setStatus(1);
+                        $delivery->getDriver()->setStatus(1);
+                    }
+                }
+            }
+            $delivery->setCreatedAt(new DateTime());
+            $manager->persist($delivery);
         }
 
 
